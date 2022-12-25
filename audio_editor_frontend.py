48a68ffe02c07c6d0d0b9d9baa2828aefe5142eb
justspace <js4std@gmail.com>
2022-12-25 22:35:17 +0500

    Same as previous

--- audio_editor_frontend.py
+++ audio_editor_frontend.py
@@ -2,6 +2,7 @@ import pygame
 from input_box import InputBox
 from button_box import ButtonBox
 from audio_editor_backend import AudioEditorBackEnd
+from dropdown import DropDown
 
 
 class AudioEditorFrontEnd:
@@ -10,11 +11,12 @@ class AudioEditorFrontEnd:
         self.height = 600
         self.font = "Georgia"
         self.font_size = 20
-        self.FPS = 60
-        self.backward = (189, 155, 207)
+        self.FPS = 30
+        self.backward = (199, 198, 173)
         self.backend = AudioEditorBackEnd()
         self.input_boxes = dict()
         self.texts = dict()
+        self.dropdown = None
         self.buttons = dict()
 
     def start(self):
@@ -26,20 +28,31 @@ class AudioEditorFrontEnd:
         self.create_texts()
         self.create_input_boxes()
         self.create_buttons()
+        self.create_dropdown()
+
+        # DropDown
 
         while True:
             screen.fill(self.backward)
-            self.handle_events()
+
+            # DropDown
+            events = pygame.event.get()
+
+            self.handle_events(events)
 
             self.draw_texts(screen)
-            self.draw_input_boxes(screen)
+            # self.draw_input_boxes(screen)
             self.draw_buttons(screen)
+            # DropDown
+            self.dropdown.draw(screen)
+            # print(dropdown.last_option)
 
             pygame.display.update()
             clock.tick(self.FPS)
 
-    def handle_events(self):
-        for event in pygame.event.get():
+    def handle_events(self, events):
+        self.dropdown_handle_event(events)
+        for event in events:
             keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -47,6 +60,11 @@ class AudioEditorFrontEnd:
                 print(f"Position: {event.pos}")
             self.boxes_handle_event(event, keys)
             self.buttons_handle_event(event)
+
+    def dropdown_handle_event(self, events):
+        selected_option = self.dropdown.update(events)
+        if selected_option > -1:
+            self.dropdown.main = self.dropdown.options[selected_option]
 
     def boxes_handle_event(self, event, keys):
         for box in self.input_boxes.keys():
@@ -76,8 +94,8 @@ class AudioEditorFrontEnd:
         font = pygame.font.SysFont(self.font, self.font_size, bold=True)
         self.texts['input_text'] = font.render('Введите название файла:', True, 'white')
         self.texts['question_text'] = font.render('Что хотим сделать?', True, 'white')
-        self.texts['acceleration_text'] = font.render('Ускорить с коэффициентом', True, 'white')
-        self.texts['change_volume_text'] = font.render('Изменить громкость с коэффициентом', True, 'white')
+        self.texts['acceleration_text'] = font.render('Ускорить с коэффициентом:', True, 'white')
+        self.texts['change_volume_text'] = font.render('Изменить громкость с коэффициентом:', True, 'white')
         self.texts['cut_from_text'] = font.render('Обрезать, начиная с', True, 'white')
         self.texts['cut_to_text'] = font.render('секунды, длительность в', True, 'white')
         self.texts['reverse_text'] = font.render('Обратить', True, 'white')
@@ -91,21 +109,54 @@ class AudioEditorFrontEnd:
         self.input_boxes["cut_to_input"] = (InputBox(555, 178, 30, 32))
 
     def create_buttons(self):
-        self.buttons['acceleration_button'] = ButtonBox(370, 98, 30, 30)
-        self.buttons['change_volume_button'] = ButtonBox(490, 137, 30, 30)
-        self.buttons['cut_button'] = ButtonBox(595, 178, 30, 30)
-        self.buttons['reverse_button'] = ButtonBox(128, 222, 30, 30)
-        self.buttons['concat_button'] = ButtonBox(600, 260, 30, 30)
+        # self.buttons['acceleration_button'] = ButtonBox(370, 98, 30, 30)
+        # self.buttons['change_volume_button'] = ButtonBox(490, 137, 30, 30)
+        # self.buttons['cut_button'] = ButtonBox(595, 178, 30, 30)
+        # self.buttons['reverse_button'] = ButtonBox(128, 222, 30, 30)
+        # self.buttons['concat_button'] = ButtonBox(600, 260, 30, 30)
+        self.buttons['execute_button'] = ButtonBox(280, 120, 50, 50)
+
+    def create_dropdown(self):
+        # DropDown
+        COLOR_INACTIVE = (100, 80, 255)
+        COLOR_ACTIVE = (100, 200, 255)
+        COLOR_LIST_INACTIVE = (255, 100, 100)
+        COLOR_LIST_ACTIVE = (255, 150, 150)
+
+        # DropDown
+        self.dropdown = DropDown(
+            [COLOR_INACTIVE, COLOR_ACTIVE],
+            [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
+            40, 120, 200, 50,
+            pygame.font.SysFont(None, 30),
+            "Выберите опцию", ["Ускорение", "Громкость", "Обрезка", "Инвертирование",
+                               "Объединение"])
 
     def draw_texts(self, screen):
-        screen.blit(self.texts['input_text'], (20, 20))
-        screen.blit(self.texts['question_text'], (20, 60))
-        screen.blit(self.texts['acceleration_text'], (20, 100))
-        screen.blit(self.texts['change_volume_text'], (20, 140))
-        screen.blit(self.texts['cut_from_text'], (20, 180))
-        screen.blit(self.texts['cut_to_text'], (20 + self.texts['cut_from_text'].get_width() + 40, 180))
-        screen.blit(self.texts['reverse_text'], (20, 220))
-        screen.blit(self.texts['concat_text'], (20, 260))
+        # screen.blit(self.texts['input_text'], (20, 20))
+        # screen.blit(self.texts['question_text'], (20, 60))
+        if self.dropdown.last_option == -1:
+            pass
+        elif self.dropdown.last_option == 0:
+            screen.blit(self.texts['acceleration_text'], (self.dropdown.rect.x,
+                                                          self.dropdown.rect.y + 70))
+        elif self.dropdown.last_option == 1:
+            screen.blit(self.texts['change_volume_text'], (self.dropdown.rect.x,
+                                                          self.dropdown.rect.y + 70))
+        elif self.dropdown.last_option == 2:
+            screen.blit(self.texts['cut_from_text'], (self.dropdown.rect.x,
+                                                          self.dropdown.rect.y + 70))
+            screen.blit(self.texts['cut_to_text'], (20 + self.texts['cut_from_text'].get_width() + 40,
+                                                    self.dropdown.rect.y + 70))
+        elif self.dropdown.last_option == 3:
+            pass
+        elif self.dropdown.last_option == 4:
+            pass
+        # screen.blit(self.texts['change_volume_text'], (20, 140))
+        # screen.blit(self.texts['cut_from_text'], (20, 180))
+        # screen.blit(self.texts['cut_to_text'], (20 + self.texts['cut_from_text'].get_width() + 40, 180))
+        # screen.blit(self.texts['reverse_text'], (20, 220))
+        # screen.blit(self.texts['concat_text'], (20, 260))
 
     def draw_input_boxes(self, screen):
         for box in self.input_boxes.keys():
@@ -115,4 +166,5 @@ class AudioEditorFrontEnd:
     def draw_buttons(self, screen):
         for button in self.buttons.keys():
             self.buttons[button].draw(screen)
+
 
